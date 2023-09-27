b2c70159eb40540c153fb09fa14c6b93e9574916
Reduan4u <reduanulhaque13@gmail.com>
2023-09-27 07:07:48 +0600

    commit after a error fixing in the donation route

--- src/Components/Home/Banner/Banner.jsx
+++ src/Components/Home/Banner/Banner.jsx
@@ -10,7 +10,7 @@ const Banner = ({ handleInputChange }) => {
                     <h1 className="mb-5 text-3xl sm:text-4xl md:text-4xl lg:text-5xl font-bold text-black">I Grow By Helping People In Need</h1>
                     <form onSubmit={handleInputChange} className="flex flex-col sm:flex-row lg:flex-row md:flex-row justify-center mx-2 mt-5 relative z-10 pt-6 gap-2 lg:gap-0 md:gap-0 sm:gap-0">
                         <input type="search" placeholder="Search here..." className=" lg:rounded-r-none sm:rounded-l-lg md:rounded-l-lg lg:rounded-l-lg py-3 px-4 text-black font-semibold" />
-                        <button style={{ backgroundColor: "#FF444A" }} className="text-xl font-semibold px-7 border-none text-white py-3 sm:rounded-r-lg md:rounded-r-lg lg:rounded-r-lg ">Search</button>
+                        <button style={{ backgroundColor: "#FF444A" }} className="text-xl font-semibold px-7 border-none text-white py-3 sm:rounded-r-lg md:rounded-r-lg lg:rounded-r-lg active:scale-95 transform transition-transform duration-200 ease-in-out">Search</button>
                     </form>
                 </div>
             </div>
